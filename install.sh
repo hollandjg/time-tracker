0185b129ec5e169e51aa4cc820af4bb956361c26
John Gerrard Holland <john_holland1@brown.edu>
2023-01-30 09:45:57 -0500

    remove extra scripts and app

--- install.sh
+++ install.sh
@@ -1,4 +1,3 @@
 #!/bin/zsh
-cp -r "Log Current Focus.app" ~/Applications/.
 cp edu.brown.ccv.jholla10.log-focus-every-5-minutes.plist ~/Library/LaunchAgents/.
 launchctl load -w ~/Library/LaunchAgents/edu.brown.ccv.jholla10.log-focus-every-5-minutes.plist
--- uninstall.sh
+++ uninstall.sh
@@ -1,4 +1,3 @@
 #!/bin/zsh
 launchctl unload -w ~/Library/LaunchAgents/edu.brown.ccv.jholla10.log-focus-every-5-minutes.plist
 rm ~/Library/LaunchAgents/edu.brown.ccv.jholla10.log-focus-every-5-minutes.plist
-rm -r ~/Applications/Log\ Current\ Focus.app
