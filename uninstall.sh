d6bfe6d954e1aca53a1137b8cc721f76daba0b59
John Gerrard Holland <john_holland1@brown.edu>
2023-01-30 09:35:39 -0500

    update install/uninstall to deal with app

--- uninstall.sh
+++ uninstall.sh
@@ -1,3 +1,4 @@
 #!/bin/zsh
 launchctl unload -w ~/Library/LaunchAgents/edu.brown.ccv.jholla10.log-focus-every-5-minutes.plist
 rm ~/Library/LaunchAgents/edu.brown.ccv.jholla10.log-focus-every-5-minutes.plist
+rm -r ~/Applications/Log\ Current\ Focus.app
